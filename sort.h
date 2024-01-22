4dd7fbb68a108c346e3742f511f0a8d69441013f
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2024-01-22 09:09:37 -0800

    .h file

--- sort.h
+++ sort.h
@@ -23,8 +23,8 @@ typedef enum bool
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer Stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @prev: Pointer to the previous Element of the list
+ * @next: Pointer to the next Element of the list
  */
 typedef struct listint_s
 {
